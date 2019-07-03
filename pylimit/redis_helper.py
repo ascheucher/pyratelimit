dbfedfbe715143f93286e1cd881d494336f684e3
Aemil Estvold <aemil@tomis.tech>
2019-07-03 13:30:11 -0600

    Update to support `redis` 3.0, replace `StrictRedis` and `StrictPipeline` with `Redis` and `Pipeline`

--- pylimit/redis_helper.py
+++ pylimit/redis_helper.py
@@ -1,11 +1,12 @@
 import redis
+from redis.client import Pipeline
 from redis.sentinel import Sentinel
-from redis.client import StrictPipeline
-import redis.client
 
 
 class RedisHelper(object):
-    def __init__(self, host: str, port: int, is_sentinel=False, sentinel_service=None, password=None):
+    def __init__(
+        self, host: str, port: int, is_sentinel=False, sentinel_service=None, password=None
+    ):
         self.host = host
         self.port = port
         self.is_sentinel = is_sentinel
@@ -15,14 +16,14 @@ class RedisHelper(object):
         self.connection = None
         self.get_connection()  # Ensure connection is established
 
-    def get_connection(self, is_read_only=False) -> redis.StrictRedis:
+    def get_connection(self, is_read_only=False) -> redis.Redis:
         """
-        Gets a StrictRedis connection for normal redis or for redis sentinel based upon redis mode in configuration.
+        Gets a Redis connection for normal redis or for redis sentinel based upon redis mode in configuration.
 
         :type is_read_only: bool
         :param is_read_only: In case of redis sentinel, it returns connection to slave
 
-        :return: Returns a StrictRedis connection
+        :return: Returns a Redis connection
         """
         if self.connection is not None:
             return self.connection
@@ -37,15 +38,16 @@ class RedisHelper(object):
             else:
                 connection = sentinel.master_for(self.sentinel_service, decode_responses=True)
         else:
-            connection = redis.StrictRedis(host=self.host, port=self.port, decode_responses=True,
-                                           password=self.password)
+            connection = redis.Redis(
+                host=self.host, port=self.port, decode_responses=True, password=self.password
+            )
         self.connection = connection
         return connection
 
-    def get_atomic_connection(self) -> StrictPipeline:
+    def get_atomic_connection(self) -> Pipeline:
         """
-        Gets a StrictPipeline for normal redis or for redis sentinel based upon redis mode in configuration
+        Gets a Pipeline for normal redis or for redis sentinel based upon redis mode in configuration
 
-        :return: Returns a StrictPipeline object
+        :return: Returns a Pipeline object
         """
         return self.get_connection().pipeline(True)
